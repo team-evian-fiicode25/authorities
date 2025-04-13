cde66831da65aff798abe3f23ffaa4aed09bede0
atomicul <soaremihai2006@gmail.com>
2025-04-13 03:13:32 +0300

    Add loading state to the auth form

--- frontend/src/routes/auth/+page.svelte
+++ frontend/src/routes/auth/+page.svelte
@@ -3,13 +3,17 @@
     import { goto } from "$app/navigation";
 
     const { form } = $props();
+
+    let isLoading = $state(false);
 </script>
 
 <form
     method="POST"
     class="bg-base-200 mx-auto my-5 box-border flex w-full max-w-md flex-col items-center p-8"
     use:enhance={() => {
+        isLoading = true;
         return (e) => {
+            isLoading = false;
             e.update();
             if (e.result.type != "error" && e.result.type != "failure") {
                 goto("/home");
@@ -20,13 +24,13 @@
     <h1 class="mb-2 text-xl font-bold">Log In</h1>
     <fieldset class="fieldset w-full">
         <legend class="fieldset-legend">Username</legend>
-        <input type="text" class="input w-full" name="username" value={form?.username ?? ""} />
+        <input disabled={isLoading} type="text" class="input w-full" name="username" value={form?.username ?? ""} />
     </fieldset>
     <fieldset class="fieldset w-full">
         <legend class="fieldset-legend">Password</legend>
-        <input type="password" class="input w-full" name="password" />
+        <input disabled={isLoading} type="password" class="input w-full" name="password" />
     </fieldset>
-    <button type="submit" class="btn btn-primary mt-4 w-full">Submit</button>
+    <button disabled={isLoading} type="submit" class="btn btn-primary mt-4 w-full">Submit</button>
     {#if form?.error}
         {#if form.error == "unknown"}
             <p class="text-error mt-2 w-full text-right">Something went wrong</p>
